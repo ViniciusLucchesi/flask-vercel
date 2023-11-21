095612918af5fb6a9310f203e738c28eb5ec7137
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-11-21 18:20:29 -0300

    Add MONGO_URI configuration

--- api/config.py
+++ api/config.py
@@ -6,4 +6,5 @@ CONFIGURATION_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 def configure(app):
+    app.config['MONGO_URI'] = os.getenv('MONGO_URI')
     FlaskDynaconf(app, extensions_list="EXTENSIONS", root_path=CONFIGURATION_PATH)
